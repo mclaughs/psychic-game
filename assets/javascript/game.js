2ef54a923319b291409cec0076c19b963fdf3351
mclaughs <mclaughsj2@gmail.com>
2017-09-02 00:35:50 -0400

    Homework 3.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 var wins = 0;
 var losses = 0;
 var guesses = 9;
+var guessArray = [];
 
 var alphArray = ["a","b","c","d","e"];
 
@@ -14,7 +15,51 @@ document.onkeyup = function(event) {
   var compRandom = alphArray[Math.floor(Math.random() * alphArray.length)];
 
   //Compare keypress to random and increment or decrement accordingly using functions.
+  //console.log(document); //Hima suggested.  I don't understand.
+  console.log(userGuess);
+  console.log(compRandom);
+  console.log(guesses)
 
-//Increment wins if equal decrement guesses if not.
-document.querySelector(#wins).innerHTML =
-//Increment losses if remaining guesses < 0.
+  if (guesses = 0) {
+    alert("guesses = " + guesses);
+    loss();
+    //Reset game.
+    return;
+  }
+  else if (userGuess === compRandom) {
+    //alert("hit");
+    hit();
+  }
+  else {
+    alert("miss");
+    miss();
+  }
+
+}
+
+  //"Hit" function.  The only one that's working.
+
+  function hit() {
+    //increment counter
+    wins++;
+    //Write out result.
+    document.querySelector("#wins").innerHTML = wins;
+    //Reset guesses counter to end loop.
+    //guesses = 0;
+  }
+
+  function miss() {
+    guesses--;
+    //Write out result.
+    document.querySelector("#remaining").innerHTML = guesses;
+    //Push out user selection to a displayable array.
+  }
+
+  function loss() {
+    losses++;
+    //Write out result.
+    document.querySelector("#losses").innerHTML = losses;
+    //Reset guesses counter to end loop.
+    //guesses = 0;
+
+  }
